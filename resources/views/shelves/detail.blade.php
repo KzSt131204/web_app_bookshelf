260c4fb1733cce27175e59cffc9348cffdec41d5
KzSt131204 <55kazutomo55@gmail.com>
2023-10-26 03:35:54 +0900

    detail追加

--- resources/views/shelves/detail.blade.php
+++ resources/views/shelves/detail.blade.php
@@ -6,6 +6,9 @@
      <h2 class='title'>{{ $book->title }}</h2>
         <h3 class='auther'>{{ $book->auther}}</h3>
         <h3 class='publisher'>{{ $book->publisher}}</h3>
-        </div>
+</div>
         
+ <div class="footer">
+        <a href="/book">戻る</a>
+    </div>
 @endsection
